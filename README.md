ab1f266064941ce0fd0fbf502198a88e1e5949ef
Luke Cordova <lukethegreat000@gmail.com>
2021-05-30 15:11:45 -0700

    updated the readme file for todos

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # MDB
-Bot to control a discord comunity
-test
+
+
+<pre>
+<h1>TO DO: </h1>
+    index.js: 
+        Needs to listen for events 
+            ready - set up the discord server. Check for certain catagories and channels on the server. 
+                Create those channels and catagories if they are not created before inviting the bot.
+            guildMemberAdd - when a member joins the server say hi and direct them to the roll assigning page
+                To assign the proper roll. 
+            message - listen for commands. 
+                <ul>COMMANDS: 
+                    <li>roll [certain roll]: this should assign a roll to the user who typed it.
+                        The idea is that we have rolls for different experience levels of programming.
+                        EX. beginner, intermediate, pro</li>
+                    <li>kick [user]: this command is only for higher up mods or owners</li>
+                    <li>ban [user]: this command is only for higher up mods or owners</li>
+                    <li>give-role [user] [roll]: this command is only for higher up mods or owners</li>
+                </ul>
+</pre>
--- src/cmds.js
+++ src/cmds.js
@@ -3,48 +3,51 @@ const Discord = require('discord.js');
 const {embed} = require('./style');
 const PFIX = '$';
 async function cmds(msg){
+
+    // roll-assign
+    console.log(msg.channel.name);
     // high auth cmds
         // kick
-    if(msg.content.startsWith(`${PFIX}kick `)){
-        const user = msg.mentions.users.first();
-        if(user){
-            const member = msg.guild.member(user);
-            if(member){
-                member
-                    .kick('Because I said')
-                    .then(() => {
-                        msg.reply(embed({title:'KICK: ', description: `Gettum outa here. See ya ${user.tag}`}));
-                    })
-                    .catch(err => {
-                        msg.reply(embed({title:'KICK: ', description: `Error: Couldnt kick ${user.tag}`}));
-                    })
-                ;
-            }else{
-                msg.reply(embed({title: 'KICK: ', description: `couldnt find that user`}));
-            }
-        }else{
-            msg.reply(embed({title: 'KICK: ', description: 'couldnt find that user'}));
-        }
-    }
-        // ban
-        // promote people
-    if(msg.content.startsWith(`${PFIX}promote `)){
-        const user = msg.mentions.users.first();
-    }
-        // delete messages
-    if(msg.content.startsWith(`${PFIX}clear `)){
-        const reg = /\b\d+/;
-        const limit = parseInt(reg.exec(msg.content))+1;
-        msg.channel.messages.fetch({ limit})
-            .then(messages => {
-                messages.every(m => m.delete());
-                console.log(`deleted ${limit}`);
-            })
-            .catch(err => {
-                console.error(err);
-            })
-        ;
-    }
+    // if(msg.content.startsWith(`${PFIX}kick `)){
+    //     const user = msg.mentions.users.first();
+    //     if(user){
+    //         const member = msg.guild.member(user);
+    //         if(member){
+    //             member
+    //                 .kick('Because I said')
+    //                 .then(() => {
+    //                     msg.reply(embed({title:'KICK: ', description: `Gettum outa here. See ya ${user.tag}`}));
+    //                 })
+    //                 .catch(err => {
+    //                     msg.reply(embed({title:'KICK: ', description: `Error: Couldnt kick ${user.tag}`}));
+    //                 })
+    //             ;
+    //         }else{
+    //             msg.reply(embed({title: 'KICK: ', description: `couldnt find that user`}));
+    //         }
+    //     }else{
+    //         msg.reply(embed({title: 'KICK: ', description: 'couldnt find that user'}));
+    //     }
+    // }
+    //     // ban
+    //     // promote people
+    // if(msg.content.startsWith(`${PFIX}promote `)){
+    //     const user = msg.mentions.users.first();
+    // }
+    //     // delete messages
+    // if(msg.content.startsWith(`${PFIX}clear `)){
+    //     const reg = /\b\d+/;
+    //     const limit = parseInt(reg.exec(msg.content))+1;
+    //     msg.channel.messages.fetch({ limit})
+    //         .then(messages => {
+    //             messages.every(m => m.delete());
+    //             console.log(`deleted ${limit}`);
+    //         })
+    //         .catch(err => {
+    //             console.error(err);
+    //         })
+    //     ;
+    // }
     
 }
 
